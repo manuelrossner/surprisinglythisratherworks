9f9ed47d48b366c3c5678ecb5f8cc96f79f6bf37
Studio Manuel Rossner <52995926+manuelrossner@users.noreply.github.com>
2020-04-04 19:38:18 +0200

    Update sculptureCollide.cs
    Added Explanations

--- sculptureCollide.cs
+++ sculptureCollide.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class sculptureCollide : MonoBehaviour
 {
 
-    public Vector3 initPos;
+    public Vector3 initPos; 
     public static bool isActive = false;
     public GameObject sculpture;
 
@@ -15,23 +15,23 @@ public class sculptureCollide : MonoBehaviour
       originalRotationValue = transform.rotation;
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision) // When the player hits the sculpture
     {
-        if (isActive == false) {
-          GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-          StartCoroutine (ResetSculpture(sculpture, initPos, originalRotationValue));
-          isActive = true;
+        if (isActive == false) { // If it hasn't been hit before
+          GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None; // Unfreeze the sculpture and let gravity work
+          StartCoroutine (ResetSculpture(sculpture, initPos, originalRotationValue)); // Start counting down to reset the sculture
+          isActive = true; 
         }
     }
 
     static IEnumerator ResetSculpture(GameObject obj, Vector3 initPos, Quaternion originalRotationValue){
 
-    	yield return new WaitForSeconds (5.0f);
+      yield return new WaitForSeconds (5.0f); // Wait for 5 seconds
 
-      obj.transform.position = initPos;
-      obj.transform.rotation = Quaternion.Slerp(obj.transform.rotation, originalRotationValue, Time.time * 1.0f);
-      obj.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+      obj.transform.position = initPos; // Reset the sculpture to it's original position
+      obj.transform.rotation = Quaternion.Slerp(obj.transform.rotation, originalRotationValue, Time.time * 1.0f); // Reset to original rotation
+      obj.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll; // Freeze the sculpture again (so it won't fall immediately)
 
-      isActive = false;
+      isActive = false; // let the software know that it's inactive again, so the player can hit it again
    }
 }
